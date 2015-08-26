f36d64e849844bc41722c43babe8fe50f516b1b0
Joel "The Merciless" Meador <joel@expectedbehavior.com>
2015-08-26 12:21:51 -0400

    Update synchronous-pdf-example.php
    update spacing per scrutinizer

--- examples/synchronous-pdf-example.php
+++ examples/synchronous-pdf-example.php
@@ -3,11 +3,11 @@
 //   composer install
 //   php -f examples/sychronous-pdf-example.php
 
-require_once(__DIR__ . '/../vendor/autoload.php');
+require_once(__DIR__.'/../vendor/autoload.php');
 
 $docRaptor = new DocRaptor\ApiWrapper("YOUR_API_KEY_HERE");
 $docRaptor->setDocumentContent('<h1>Hello!</h1>')->setDocumentType('pdf')->setTest(true)->setName('synchronous-pdf-example.pdf');
 $file = $docRaptor->fetchDocument();
-$file_location = __DIR__ . '/test.pdf';
+$file_location = __DIR__.'/test.pdf';
 file_put_contents($file_location, $file);
 echo(sprintf("File written to: %s\n", $file_location));
